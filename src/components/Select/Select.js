a5cfa3d1cb263d46fb64aae07b8be6a62b887841
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-03-27 23:33:57 -0500

    select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import React from "react"
+import { tw } from "twind"
+import Icon from "../Icon"
+import { getDisplayedValue } from "./Select.helpers"
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValue = getDisplayedValue(value, children)
+
+  const containerStyle = tw`
+    relative
+    bg-transparentGray15
+    text-gray700
+    pl-4 py-3 pr-12
+    rounded-lg
+    w-[fit-content]
+    hover:text-black
+  `
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <div className={containerStyle}>
+      <select
+        className={tw`absolute top-0 left-0 w-full h-full bg-[transparent] appearance-none px-4 py-3`}
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </select>
+      <div className={tw`w-[fit-content] invisible`}>{displayedValue}</div>
+      <div
+        className={tw`absolute right-3 top-0 bottom-0 my-auto h-[min-content] pointer-events-none`}
+      >
+        <Icon id="chevron-down" />
+      </div>
+    </div>
+  )
+}
 
-export default Select;
+export default Select
--- src/twind.config.js
+++ src/twind.config.js
@@ -1,11 +1,16 @@
 // @ts-check
 import { COLORS } from "./constants"
 
+/** @type {import('twind').Configuration} */
 const config = {
   theme: {
     colors: COLORS,
     boxShadow: {
       inset: `inset 0px 2px 4px rgba(128, 128, 128, 0.35);`,
+    },
+    extend: {
+      ringWidth: { 3: "3px" },
+      ringOffsetWidth: { 3: "3px" },
     },
   },
 }
